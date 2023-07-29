9e11e927d83ab9622561aa6124bf661b1f2aa5d8
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-07-30 01:18:40 +0530

    Making responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
-      <Button colorScheme='cyan' variant='outline'>Submit</Button>
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }}>
+        <GridItem area={"nav"} bg="red.300">Nav</GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg="green.300">Aside</GridItem>
+        </Show>
+        <GridItem area={"main"} bg="blackAlpha.500">Main</GridItem>
+      </Grid>
     </>
   )
 }
