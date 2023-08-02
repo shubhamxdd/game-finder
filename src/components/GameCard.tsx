588a000697f33966e09bf016bc99b1c63a769e56
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-02 12:35:23 +0530

    Fetch optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../types/gameTypes";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCorppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image} />
+        <Image src={getCorppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
           <HStack justifyContent="space-between">
---
+++ src/services/image-url.ts
@@ -0,0 +1,6 @@
+const getCorppedImageUrl = (url: string) => {
+  const index = url.indexOf("media/") + "media/".length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCorppedImageUrl;
