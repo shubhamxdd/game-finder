e9e596ece61510697ad4227444b33984d324583d
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-03 01:00:48 +0530

    Making useData and useGames hooks more flexible

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,29 +1,39 @@
 import { useEffect, useState } from "react";
 import { FetchGenres } from "../types/genreTypes";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    apiClient
-      .get<FetchGenres<T>>(endpoint, { signal: controller.signal })
-      .then((res) => {
-        setLoading(false);
-        setData(res.data.results);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      setLoading(true);
+      apiClient
+        .get<FetchGenres<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setLoading(false);
+          setData(res.data.results);
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setError(error.message);
+          setLoading(false);
+        });
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
   return { data, error, isLoading };
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,10 @@
 import { Game } from "../types/gameTypes";
+import { Genre } from "../types/genreTypes";
 import useData from "./useData";
 
-const useGames = () => useData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGames;
