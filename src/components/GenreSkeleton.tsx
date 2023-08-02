188777b578c19b37968a55b0db51a52c37e6bc4b
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-03 00:19:54 +0530

    Make genre skeleton better

--- src/components/GenreSkeleton.tsx
+++ src/components/GenreSkeleton.tsx
@@ -1,10 +1,13 @@
-import { ListItem, Skeleton } from "@chakra-ui/react";
+import { ListItem, Skeleton, SkeletonText, Stack } from "@chakra-ui/react";
 
 const GenreSkeleton = () => {
   return (
     <>
       <ListItem paddingY={"5px"}>
-        <Skeleton boxSize="32px" borderRadius={8} />
+        <Stack direction="row" alignItems="center">
+          <Skeleton boxSize="32px" borderRadius={8} />
+          <SkeletonText ml="10px" noOfLines={1} w="100px" />
+        </Stack>
       </ListItem>
     </>
   );
