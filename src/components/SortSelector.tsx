95cb9a0356b103edb28ef2e3ccb925eb293f8085
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-04 09:16:25 +0530

    Sort games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,20 +1,38 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { AiOutlineDown } from "react-icons/ai";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrderN: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrderN }: Props) => {
+  const sortOrder = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
   return (
     // <Box marginX={3}>
     <Menu>
       <MenuButton as={Button} rightIcon={<AiOutlineDown />}>
-        Order by
+        Order by : {sortOrder.find((sort) => sort.value === sortOrderN)?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>1</MenuItem>
-        <MenuItem>2</MenuItem>
-        <MenuItem>3</MenuItem>
-        <MenuItem>4</MenuItem>
-        <MenuItem>5</MenuItem>
-        <MenuItem>6</MenuItem>
+        {sortOrder.map((sort) => {
+          return (
+            <MenuItem
+              key={sort.value}
+              value={sort.value}
+              onClick={() => onSelectSortOrder(sort.value)}
+            >
+              {sort.label}
+            </MenuItem>
+          );
+        })}
       </MenuList>
     </Menu>
     // </Box>
