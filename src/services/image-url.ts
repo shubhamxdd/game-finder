9624fcfe67c18072d28e65fb26c73351ddfc0c8e
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-04 09:21:57 +0530

    Add image for games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCorppedImageUrl = (url: string) => {
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
