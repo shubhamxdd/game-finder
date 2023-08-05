c37f43814a5abfcc430e4718ad2db6ef87e23b98
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-05 08:24:59 +0530

    Customize theme

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,9 +12,10 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  if (error) return <Text color="tomato">{error}</Text>;
+
   return (
     <>
-      {error && <Text color="tomato">{error}</Text>}
       <SimpleGrid
         columns={{
           sm: 1,
